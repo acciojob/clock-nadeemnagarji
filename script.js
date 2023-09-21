bc1585f11a4994241b1f998260df6eec4f488eb1
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-21 12:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const display = document.getElementById("timer")
+	setInterval(()=>{
+		const date = new Date()
+				display.innerText = `${date.toLocaleDateString()}, ${date.toLocaleTimeString()}`
+},1000)
